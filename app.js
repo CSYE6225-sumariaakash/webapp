4f5e4002bf98596a609bb06133eedb71c57e983b
SumariaAK <sumaria.ak@northeastern.edu>
2022-10-19 01:13:25 -0400

    Assign2

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import router from './src/route/routhealthz.js';
 import bodyParser from 'body-parser';
+import db from './src/models/index.js';
+import user_router from './src/route/userRoute.js';
 
 const app = express();
 //either use env variable port or 3000
 const port = process.env.PORT || 3000;
+
+db.sequelize.sync();
 
 //parsing incoming request bodies
 app.use(bodyParser.urlencoded({ extended: true }))
 //json used
 app.use(bodyParser.json())
 app.use(router);
+
+app.use(user_router);
 
 //landing page
 app.get('/', (req, res) => {
---
+++ src/models/index.js
@@ -0,0 +1,25 @@
+import dbConfig from '../config//dbconfig.js';
+import Sequelize from 'sequelize';
+import Users from './user_model.js';
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = Users(sequelize, Sequelize);
+
+export default db;
